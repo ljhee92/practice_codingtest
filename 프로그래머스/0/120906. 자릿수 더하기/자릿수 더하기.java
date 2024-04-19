4da1566a5d65e74b7a764a528a79ca5c4adcfae4
주히 <77716414+ljhee92@users.noreply.github.com>
2024-04-19 12:53:18 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.16 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,11 +2,9 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         String[] strs = String.valueOf(n).split("");
-        int[] numbers = new int[strs.length];
         
         for(int i = 0; i < strs.length; i++) {
-            numbers[i] = Integer.parseInt(strs[i]);
-            answer += numbers[i];
+            answer += Integer.parseInt(strs[i]);
         }
         return answer;
     }
