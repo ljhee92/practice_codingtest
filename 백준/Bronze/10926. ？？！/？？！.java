ac2400deac809f99f343df75d15be727f79df3b8
주히 <77716414+ljhee92@users.noreply.github.com>
2024-10-28 18:45:16 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        StringBuilder result = new StringBuilder(input);
+        System.out.println(result.append("??!").toString());
+    }
+}
