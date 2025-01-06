4b7d9a80074876dc081db1fbcda7ea33572fc93e
juhee <77716414+ljhee92@users.noreply.github.com>
2025-01-06 18:25:25 +0900

    [Bronze II] Title: 분해합, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,33 +1,32 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        br.close();
-        
-        int m = 0;
-        int number = 0;
-        
-        for (int i = 1; i <= n; i++) {
-            number = i;
-            int sum = 0; 
-            
-            while (number > 0) {
-                sum += number % 10;
-                number /= 10;
-            }
-            
-            if (i + sum == n) {
-                m = i;
-                break;
-            }
-        }
-        
-        bw.write(m + "\n");
-        bw.flush();
-        bw.close();
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String nStr = br.readLine();
+		int n = Integer.parseInt(nStr);
+		int start = n - (nStr.length() * 9);
+		int m = 0;
+		
+		for (int i = start; i <= n; i++) {
+			int sum = 0; 
+			int number = i;
+			
+			while (number != 0) {
+				sum += number % 10;
+				number /= 10;
+			}
+			
+			if (i + sum == n) {
+				m = i;
+				break;
+			}
+		}
+		
+		bw.write(m + "\n");
+		bw.flush();
+		bw.close();
+	}
 }
