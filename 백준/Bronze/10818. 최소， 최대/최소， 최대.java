cc3abe03f870ce2c60d07c0dd2aafa5a21a90d5c
juhee <77716414+ljhee92@users.noreply.github.com>
2025-02-06 17:35:08 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 91948 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
