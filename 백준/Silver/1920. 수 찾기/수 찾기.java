f92dceb5358b14a255430df4e814cafe221c0350
juhee <77716414+ljhee92@users.noreply.github.com>
2025-03-12 09:27:55 +0900

    [Silver IV] Title: 수 찾기, Time: 1096 ms, Memory: 60616 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,19 +4,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        HashSet<Integer> A = new HashSet<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A.add(Integer.parseInt(st.nextToken()));
-        }
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int number = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> numbers = new HashSet<>();
+        String a = br.readLine();
+        StringTokenizer st = new StringTokenizer(a);
+        
+        for (int i = 0; i < n; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+        
+        int m = Integer.parseInt(br.readLine());
+        String line = br.readLine();
+        st = new StringTokenizer(line);
+        
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
             int answer = 0;
-            if (A.contains(number)) {
+            if (numbers.contains(num)) {
                 answer = 1;
             }
             System.out.println(answer);
