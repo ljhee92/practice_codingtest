84bd7b7d462a955f688e26e57df5412b8b9805a6
juhee <77716414+ljhee92@users.noreply.github.com>
2025-07-13 14:48:16 +0900

    [Bronze V] Title: 새싹, Time: 104 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        String answer = """
+         ,r'"7
+r`-_   ,'  ,/
+ \\. ". L_r'
+   `~\\/
+      |
+      |
+        """;
+        System.out.println(answer);
+    }    
+}
