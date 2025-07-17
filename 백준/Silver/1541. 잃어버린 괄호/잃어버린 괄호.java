3c18358984076f3c4fe3e624b1594e4472029fde
juhee <77716414+ljhee92@users.noreply.github.com>
2025-07-17 18:22:58 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer sub = new StringTokenizer(br.readLine(), "-");
+        int sum = Integer.MAX_VALUE;
+        
+        while (sub.hasMoreTokens()) {
+            int temp = 0;
+            StringTokenizer add = new StringTokenizer(sub.nextToken(), "+");
+            
+            while (add.hasMoreTokens()) {
+                temp += Integer.parseInt(add.nextToken());
+            }
+            
+            if (sum == Integer.MAX_VALUE) {
+                sum = temp;
+            } else {
+                sum -= temp;
+            }
+        }
+        
+        System.out.println(sum);
+    }    
+}
