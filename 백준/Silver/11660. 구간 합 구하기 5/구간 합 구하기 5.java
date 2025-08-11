6f7a8547b5e03a550449da264845c0d5c5843949
juhee <77716414+ljhee92@users.noreply.github.com>
2025-08-11 13:39:52 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 736 ms, Memory: 122980 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,6 +4,9 @@ import java.util.*;
 public class Main {
     
     public static void main(String[] args) throws Exception {
+        // 시간제한 1초 -> 1억 번 연산 가능
+        // N 최대값 1024 -> n^2 약 100만
+        // M 최대값 100,000 -> m * n^2 약 천억 -> 누적합 필요
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
@@ -17,21 +20,31 @@ public class Main {
             }
         }
 
+        // 1 3 6 10
+        // 12 15 19 24
+        // 27 31 36 42
+        // 46 51 57 64
+
+        // 1 3
+        // 6 10
         int[][] sumArr = new int[n+1][n+1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j] - sumArr[i-1][j-1] + arr[i][j];
+                sumArr[i][j] = sumArr[i-1][j] + sumArr[i][j-1] - sumArr[i-1][j-1] + arr[i][j];
             }
         }
 
-        for (int i = 1; i <= m; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            int answer = sumArr[x2][y2] - sumArr[x1-1][y2] - sumArr[x2][y1-1] + sumArr[x1-1][y1-1];
-            System.out.println(answer);
+
+            int sum = sumArr[x2][y2] - sumArr[x1-1][y2] - sumArr[x2][y1-1] + sumArr[x1-1][y1-1];
+            sb.append(sum).append("\n");
         }
+        System.out.println(sb);
     }
 }
