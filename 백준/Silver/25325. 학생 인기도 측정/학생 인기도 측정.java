58d1be5353a4f29041558412eaa3145b536af163
juhee <77716414+ljhee92@users.noreply.github.com>
2025-08-11 12:44:42 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 156 ms, Memory: 15432 KB -BaekjoonHub

--- 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.java
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.java
@@ -1,34 +1,51 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 학생 수
         int n = Integer.parseInt(br.readLine());
+        
+        // 학생 이름이 공백으로 구분된 문자열 A
+        List<String> names = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            map.put(st.nextToken(), 0);
+            names.add(st.nextToken());
         }
-        
-        for (int i = 0; i < n; i++) {
+
+        Map<String, Integer> answer = new HashMap<>();
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            answer.put(name, 0);
+        }
+
+        for (int i = 0; i < answer.size(); i++) {
             st = new StringTokenizer(br.readLine());
             while (st.hasMoreTokens()) {
                 String liked = st.nextToken();
-                map.put(liked, map.get(liked) + 1);
+                answer.put(liked, answer.get(liked)+1);
             }
         }
         
-        List<String> names = new ArrayList<>(map.keySet());
-        names.sort((a, b) -> {
-            int diff = map.get(b) - map.get(a);
-            if (diff == 0) return a.compareTo(b);
-            return diff;
-        });
-        
-        for (String name : names) {
-            System.out.println(name + " " + map.get(name));
+        Map<String, Integer> sorted = answer.entrySet().stream()
+        .sorted(
+            Map.Entry.<String, Integer>comparingByValue(Comparator.reverseOrder())
+                .thenComparing(Map.Entry.comparingByKey())
+        )
+        .collect(Collectors.toMap(
+            Map.Entry::getKey,
+            Map.Entry::getValue,
+            (e1, e2) -> e1,
+            LinkedHashMap::new
+        ));
+
+        StringBuilder sb = new StringBuilder();
+        for (String name : sorted.keySet()) {
+            sb.append(name).append(" ").append(sorted.get(name)).append("\n");
         }
+        System.out.println(sb);
     }
 }
