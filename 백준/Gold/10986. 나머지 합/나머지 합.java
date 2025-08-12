bec919b763e46d29933e7bafc73fa281ab5a69f9
juhee <77716414+ljhee92@users.noreply.github.com>
2025-08-12 13:49:53 +0900

    [Gold III] Title: 나머지 합, Time: 452 ms, Memory: 116868 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -4,33 +4,37 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // 시간제한 1초 -> 1억 번
+        // n 최대 1,000,000
+        // m 최대 1,000
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
-        long[] sum = new long[n];
-        long[] count = new long[m];
-        long answer = 0;
 
+        // 1 3 6 7 9
+        // 1 0 0 1 0
+        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        sum[0] = Integer.parseInt(st.nextToken());
+        arr[0] = Integer.parseInt(st.nextToken()) % m;
+        long answer = 0;
         for (int i = 1; i < n; i++) {
-            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+            arr[i] = (arr[i-1] + Integer.parseInt(st.nextToken())) % m;
         }
 
+        long[] count = new long[m]; // 0 1 2
         for (int i = 0; i < n; i++) {
-            int mod = (int) (sum[i] % m);
-            if (mod == 0) answer++;
-            count[mod]++;
+            if (arr[i] == 0) {
+                answer++;
+            } 
+            count[arr[i]]++;
         }
 
         for (int i = 0; i < m; i++) {
-            if (count[i] > 1) {
-                answer = answer + (count[i] * (count[i]-1) / 2);
+            if (count[i] > 1) { 
+                answer += (count[i] * (count[i] - 1)) / 2;
             }
         }
-
         System.out.println(answer);
     }
 }
