eb1e4be1ff19e469c971d27c27670f4ba3f90c02
juhee <77716414+ljhee92@users.noreply.github.com>
2025-08-24 17:47:20 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 596 ms, Memory: 141948 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,44 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, answer;
+
+    static int n, m;
     static List<Integer>[] list;
     static boolean[] visited;
-    
+
     public static void main(String[] args) throws Exception {
+        // 3초 -> 3억
+        // n 최대 1,000
+        // m 최대 500*999 = 499,500
+        // n * m = 499,500,000 -> 불가
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        list = new ArrayList[N+1];
-        visited = new boolean[N+1];
-        
-        for (int i = 0; i < N+1; i++) {
+        n = Integer.parseInt(st.nextToken()); // 정점
+        m = Integer.parseInt(st.nextToken()); // 노드
+
+        list = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
         }
-        
-        for (int i = 0; i < M; i++) {
+
+        visited = new boolean[n+1];
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             list[a].add(b);
             list[b].add(a);
         }
-        
-        for (int i = 1; i <= N; i++) {
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
                 dfs(i);
                 answer++;
             }
         }
-        
         System.out.println(answer);
     }
-    
+
     private static void dfs(int start) {
         visited[start] = true;
+        
         for (int num : list[start]) {
             if (!visited[num]) {
                 dfs(num);
