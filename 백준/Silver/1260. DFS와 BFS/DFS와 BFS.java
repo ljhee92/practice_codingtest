1531942b571aaf0e7d321242732ea1fe60990587
juhee <77716414+ljhee92@users.noreply.github.com>
2025-09-01 19:33:05 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 20260 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,69 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int n, m, v;
+    static List<Integer>[] list;
     static StringBuilder sb = new StringBuilder();
     static boolean[] visited;
-    static int[][] arr;
-    static int node, line, start;
-    static Queue<Integer> q = new LinkedList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken()); // 정점의 개수
+        m = Integer.parseInt(st.nextToken()); // 간선의 개수
+        v = Integer.parseInt(st.nextToken()); // 탐색 시작 정점
 
-        node = Integer.parseInt(st.nextToken()); 
-        line = Integer.parseInt(st.nextToken()); 
-        start = Integer.parseInt(st.nextToken()); 
+        list = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
 
-        arr = new int[node+1][node+1];
-
-        for (int i = 0; i < line; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            
-            arr[a][b] = arr[b][a] = 1; // 양방향 간선
+            list[a].add(b);
+            list[b].add(a);
         }
 
-        visited = new boolean[node+1];
-        dfs(start);
-        sb.append("\n");
-        
-        visited = new boolean[node+1];
-        bfs(start);
+        // 정점 번호가 작은 것을 먼저 방문 -> 정렬
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(list[i]);
+        }
 
+        visited = new boolean[n+1];
+        dfs(v);
+        sb.append("\n");
+
+        Arrays.fill(visited, false);
+        bfs(v);
+        
         System.out.println(sb);
     }
 
-    private static void dfs(int node) {
-        visited[node] = true; // 현재 노드 방문 처리
-        sb.append(node + " "); // 방문 노드 출력
-        
-        if (node == arr.length) {
-            return;
-        }
-
-        for (int i = 1; i < arr.length; i++) {
-            // 현재 정점과 연결된 정점 중 아직 방문하지 않은 정점일 경우
-            if (arr[node][i] == 1 && !visited[i]) {
-                dfs(i);
+    static void dfs(int start) {
+        visited[start] = true;
+        sb.append(start).append(" ");
+        for (int num : list[start]) {
+            if (!visited[num]) {
+                dfs(num);
             }
         }
     }
 
-    private static void bfs(int node) {
-        q.add(node); // 시작 정점 큐에 먼저 추가
-        visited[node] = true; // 현재 정점 방문 처리
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
 
-        while (!q.isEmpty()) {
-            node = q.poll();
-            sb.append(node + " "); // 큐에서 꺼낸 노드 출력
-            
-            for (int i = 1; i < arr.length; i++) {
-                // 큐에서 꺼낸 정점과 연결된 정점이며 아직 방문하지 않았을 때
-                if (arr[node][i] == 1 && !visited[i]) {
-                    q.add(i); // 연결된 정점 큐에 추가
-                    visited[i] = true; // 연결된 정점 방문 처리
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            sb.append(current).append(" ");
+
+            for (int num : list[current]) {
+                if (!visited[num]) {
+                    visited[num] = true;
+                    queue.add(num);
                 }
             }
         }
