19197920edae3ffe52537331051a00a7ea922ec2
juhee <77716414+ljhee92@users.noreply.github.com>
2025-09-02 09:55:48 +0900

    [Silver IV] Title: 수 찾기, Time: 564 ms, Memory: 42140 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,29 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
     public static void main(String[] args) throws Exception {
+        // 1초 -> 1억 번
+        // n, m 최대 100,000 -> 10,000,000,000 -> n^2 불가
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        Set<Integer> numbers = new HashSet<>();
-        String a = br.readLine();
-        StringTokenizer st = new StringTokenizer(a);
-        
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            numbers.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        
+        Arrays.sort(arr);
+
         int m = Integer.parseInt(br.readLine());
-        String line = br.readLine();
-        st = new StringTokenizer(line);
-        
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            int answer = 0;
-            if (numbers.contains(num)) {
-                answer = 1;
+            int target = Integer.parseInt(st.nextToken());
+            int index = Arrays.binarySearch(arr, target);
+            if (index >= 0) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
             }
-            System.out.println(answer);
         }
+        System.out.println(sb);
     }
 }
