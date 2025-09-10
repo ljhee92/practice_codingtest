4567412c7faec28c3939b3e7caac520093ebe989
juhee <77716414+ljhee92@users.noreply.github.com>
2025-09-10 23:08:54 +0900

    [Silver IV] Title: 문자열 집합, Time: 2772 ms, Memory: 31284 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        // 6초 -> 6억 번
+        // n, m 최대값 10,000 -> n^2 m^2 n*m 가능
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 문자열의 개수 n, m
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<String> s = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            s.add(br.readLine());
+        }
+
+        int answer = 0;
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            if (s.contains(str)) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
