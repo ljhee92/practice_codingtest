4f99339223c5bf766d76573ad43ad21faf650d14
juhee <77716414+ljhee92@users.noreply.github.com>
2025-09-13 01:04:10 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 752 ms, Memory: 66420 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,42 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static boolean[] visited;
-    static int[] parents;
-    static List<List<Integer>> list = new ArrayList<>();
+    static int[] answer;
+    static List<List<Integer>> tree = new ArrayList<>();
     
     public static void main(String[] args) throws Exception {
+        // 1초 -> 1억 번
+        // n 최대 100,000 -> n^2 불가
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int node = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        // 1 - 6 - 3 - 5
+        //   - 4 - 2
+        //       - 7
+        // 4 6 1 3 1 4
+
+        // 1 - 2 - 4 - 7
+        //           - 8
+        //   - 3 - 5 - 9
+        //           - 10
+        //       - 6 - 11
+        //           - 12
+        // 1 1 2 3 3 4 4 5 5 6 6
+
         StringTokenizer st;
-        
-        for (int i = 0; i <= node; i++) {
-            list.add(new ArrayList<>());
+        for (int i = 0; i <= n; i++) {
+            tree.add(new ArrayList<>());
         }
-        
-        for (int i = 0; i < node - 1; i++) {
+
+        for (int i = 0; i < n-1; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list.get(a).add(b);
-            list.get(b).add(a);
+            tree.get(a).add(b); 
+            tree.get(b).add(a);
         }
-        
-        visited = new boolean[node+1];
-        parents = new int[node+1];
-        
+
+        visited = new boolean[n+1];
+        answer = new int[n+1];
         dfs(1);
-        for (int i = 2; i <= node; i++) {
-            System.out.println(parents[i]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= n; i++) {
+            sb.append(answer[i]).append("\n");
         }
+        System.out.println(sb);
     }
-    
-    private static void dfs(int currentNode) {
-        visited[currentNode] = true;
-        
-        for (int child : list.get(currentNode)) {
+
+    private static void dfs(int node) {
+        visited[node] = true;
+
+        for (int child : tree.get(node)) {
             if (!visited[child]) {
-                parents[child] = currentNode;
+                answer[child] = node;
                 dfs(child);
             }
         }
