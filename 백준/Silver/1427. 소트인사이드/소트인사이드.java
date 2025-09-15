384cfa7e0e1ab9cb97d67aab459670b85ee61aab
juhee <77716414+ljhee92@users.noreply.github.com>
2025-09-16 00:31:53 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -5,15 +5,12 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split("");
-        Integer[] numbers = new Integer[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            numbers[i] = Integer.parseInt(arr[i]);
-        }
+        String number = br.readLine();
+        String[] numbers = number.split("");
         Arrays.sort(numbers, Comparator.reverseOrder());
 
         StringBuilder sb = new StringBuilder();
-        for (int num : numbers) {
+        for (String num : numbers) {
             sb.append(num);
         }
         System.out.println(sb);
