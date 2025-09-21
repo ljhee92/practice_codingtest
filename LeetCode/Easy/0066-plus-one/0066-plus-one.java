30b7a5214d50c3c1eb41f020d244171327c7d4ae
juhee <77716414+ljhee92@users.noreply.github.com>
2025-09-21 09:51:06 +0900

    Time: 0 ms (100%), Space: 42.2 MB (18.21%) - LeetHub

--- LeetCode/Easy/0066-plus-one/0066-plus-one.java
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.java
@@ -2,17 +2,16 @@ import java.math.BigInteger;
 
 class Solution {
     public int[] plusOne(int[] digits) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < digits.length; i++) {
-            sb.append(digits[i]);
-        }
-        BigInteger num = new BigInteger(sb.toString());
-        BigInteger plusOne = num.add(BigInteger.ONE);
-        String[] strs = String.valueOf(plusOne).split("");
-        int[] answer = new int[strs.length];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = Integer.parseInt(strs[i]);
-        }
-        return answer;
+            for (int i = digits.length-1; i >= 0; i--) {
+                if (digits[i] < 9) {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+
+            int[] answer = new int[digits.length+1];
+            answer[0] = 1;
+            return answer;
     }
 }
